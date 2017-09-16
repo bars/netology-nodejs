863a2978fa462e61de7dd493a390f0ab6b04d3f3
Bars88 <barser88@gmail.com>
2017-09-17 00:11:35 +0300

    сделал допы

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 hidenseek = require('./hidenseek');
 fs = require('fs');
 
-pokemonList = ['charm', 'pika', 'meow', 'bulbazaur'];
 if (process.argv[2] === 'hide')
 {
     fs.readFile(process.argv[4], (err, content) => {
@@ -9,8 +8,26 @@ if (process.argv[2] === 'hide')
         {
             throw err;
         }
-        hidenseek.hide(process.argv[3], JSON.parse(content));
+        fs.exists(process.argv[3], (exists) => {
+            if (exists)
+            {
+                hidenseek.hide(process.argv[3], JSON.parse(content));
+            }
+            else
+            {
+                fs.mkdir(process.argv[3], err => {
+                if (err)
+                {
+                    throw err;
+                }
+                hidenseek.hide(process.argv[3], JSON.parse(content));
+                })
+            }
+        })
+        
+    
     })
+    
     
 }
 if (process.argv[2] === 'seek')
